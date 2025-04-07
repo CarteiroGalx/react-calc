43d94805d6f50ede4393b206499ed0e3674516a1
Erivelton Almeida <almeidalopeserivelton@gmail.com>
2025-04-06 23:39:56 -0300

    Preparando o projeto

---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color:rgb(61, 61, 61);
+    width: 100%;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: rgb(204, 204, 204);
+    width: 80%;
+    min-height: 350px;
+`
